3e5d726bc5e311ddb942934b89ca8a62d8eeb72e
Camilo Barrios Villamizar <malditagaseosa@gmail.com>
2021-03-16 20:19:56 -0500

    Uso de componente de listas

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css';
+import List from './List';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
+  let lists = [
+    {title: "Lista 1"},
+    {title: "Lista 2"}
+  ];
   return (
-    <h1>App</h1>
+    <Container className="mt-4">
+      { lists.map((item) => {
+        return (<List title={item.title} />)
+      }) }
+    </Container>
   );
 }
 
