e3bafbbd07e71e3e448e2a06af88aa9914b10298
Camilo Barrios Villamizar <malditagaseosa@gmail.com>
2021-03-22 14:20:47 -0500

    importar estilos globales en index

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css';
 import List from './List';
 import { Container, Row, Col, Alert } from 'react-bootstrap';
